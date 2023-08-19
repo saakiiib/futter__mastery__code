ed781a1e6388866c8f21af2df30dc9ba5b91381f
saakiiib <128025804+saakiiib@users.noreply.github.com>
2023-08-19 12:53:08 +0600

    Implemented operators

---
+++ Module-01/Assignment/Operators.dart
@@ -0,0 +1,24 @@
+//Declare two constant variables "a" and "b" with any integer values of your choice.
+// Perform addition, subtraction, multiplication, division, and modulus directly on "a" and "b".
+// Print the results of each operation to the console using appropriate messages.
+void main(){
+
+  const int a = 7;
+  const int b = 3;
+
+  int addResult = a + b;
+  print('Addition:  $a + $b = $addResult');
+
+  int subResult = a - b;
+  print('Subtraction:  $a - $b = $subResult');
+
+  int mulResult = a * b;
+  print('Multiplication:  $a * $b = $mulResult');
+
+  double divResult = a / b;
+  print('Division:  $a / $b = $divResult');
+
+  int modResult = a % b;
+  print('Modulus:  $a % $b = $modResult');
+
+}
